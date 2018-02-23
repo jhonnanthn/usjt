e0b7997cb4fb8030d15c82e4c7dfb4b647755bf0
jhonnanthn.balsas <jhonnanthn.balsas@assai.com.br>
2018-02-23 09:23:51 -0300

    Colocando meu projeto - Criacao Classe Fila

---
+++ br.usjt.ftce.arqsw/src/entity/Fila.java
@@ -0,0 +1,27 @@
+package entity;
+
+public class Fila {
+	private int id;
+	private String nome;
+	
+	public Fila(int id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+	public Fila() {
+		super();
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
